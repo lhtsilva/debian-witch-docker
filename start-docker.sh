fa63bcfd1e206111b49cd0786c80bf23b680b48f
Leandro Henrique <tleandro.henrique@gmail.com>
2024-07-03 23:36:57 -0300

    Create start-docker.sh

---
+++ start-docker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Iniciar o Docker daemon
+dockerd &
+
+# Esperar até que o Docker daemon esteja pronto
+while (! docker stats --no-stream ); do
+  echo "Aguardando o Docker daemon iniciar..."
+  sleep 1
+done
+
+# Manter o container rodando
+tail -f /dev/null
